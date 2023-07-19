8648dceb890ac01b17bd4f960df119c05ef1675d
rkato24 <ryokakato24@gmail.com>
2023-07-20 06:02:19 +0900

    ダミー値を変更

--- app/src/main/java/com/example/dinergate/data/DinerGateUiState.kt
+++ app/src/main/java/com/example/dinergate/data/DinerGateUiState.kt
@@ -8,7 +8,8 @@ data class DinerGateUiState (
     val hotPepperApiResult: HotPepperApiResult,
     val shopFocusIndex: Int = 0,
     //val searchResultPage: Int = 1
-    val currentLatitude: Double = 34.8424,
+    //val currentLatitude: Double = 34.8424,
+    val currentLatitude: Double = 34.00,
     val currentLongitude: Double = 135.7895,
     val rangeId: Int = 3
 )
