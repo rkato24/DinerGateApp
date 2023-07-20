c15f764931a8b68ca58e713574ec373a48d7a069
rkato24 <ryokakato24@gmail.com>
2023-07-20 17:35:01 +0900

    コメントの追加・修正、不要なインポートの削除

--- app/src/main/java/com/example/dinergate/ui/theme/screens/DinerGateScreen.kt
+++ app/src/main/java/com/example/dinergate/ui/theme/screens/DinerGateScreen.kt
@@ -1,15 +1,9 @@
 package com.example.dinergate.ui.theme.screens
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import android.content.Context
-import android.content.Intent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.location.Location
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,16 +16,20 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.dinergate.dummyResult
+import com.example.dinergate.network.GpsLocation
+
+/*
+
+DinerGateApp : MainActivityからコールされる関数。
+各種画面へのナビゲートおよびvielModelによる変数の保持や位置情報の取得を行う。
+
+ */
 
 enum class DinerGateScreen() {
     Start,
@@ -45,13 +43,22 @@ fun DinerGateApp(
     viewModel: DinerGateViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
-
     // Get current back stack entry
     val backStackEntry by navController.currentBackStackEntryAsState()
     // Get the name of the current screen
     val currentScreen = DinerGateScreen.valueOf(
         backStackEntry?.destination?.route ?: DinerGateScreen.Start.name
     )
+    val gpsLocation = GpsLocation()
+    val currentLocation: Location? = gpsLocation.getCurrentLocation()
+    //不具合：Gpsから現在位置を取得できておらず、currentLocationがnullのままになっている
+    //一時的な対応として、GpsLocationで得られる緯度・経度の代わりに
+    //ハードコードされた値をviewModelに渡す
+    //本来は下の2行のように書きたい
+    //viewModel.setCurrentLatitude(currentLocation.getLatitude())
+    //viewModel.setCurrentLongitude(currentLocation.getLongitude())
+    viewModel.setCurrentLatitude(34.8424)
+    viewModel.setCurrentLongitude(135.7895)
 
     Scaffold(
         topBar = {
@@ -65,7 +72,6 @@ fun DinerGateApp(
     ) {innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
         val apiViewModel: HotPepperApiViewModel = viewModel()
-        apiViewModel.getHotPepperApiResult( lat = 34.8424, lng = 135.7895 )
 
         NavHost(
             navController = navController,
@@ -82,19 +88,22 @@ fun DinerGateApp(
                     }
                 }
                 StartScreen(
-                    hotPepperApiResult = uiState.hotPepperApiResult,
+                    onSelectionChanged = { viewModel.setRangeId(it) },
                     onSearchButtonClicked = {
-
+                        apiViewModel.getHotPepperApiResult(
+                            lat = uiState.currentLatitude,
+                            lng = uiState.currentLongitude,
+                            range = uiState.rangeId )
                         navController.navigate(DinerGateScreen.SearchResult.name)
                     },
-                    onSelectionChanged = { viewModel.setRangeId(it) },
-                    Id = uiState.rangeId
                 )
             }
 
             composable(route = DinerGateScreen.SearchResult.name) {
                 SearchResultScreen(
                     HPApiSearchResult = uiState.hotPepperApiResult,
+                    lat = uiState.currentLatitude,
+                    lng = uiState.currentLongitude,
                     onCardClicked = {
                         viewModel.setShopFocusIndex(it)
                         navController.navigate(DinerGateScreen.ShopDetail.name)
@@ -104,7 +113,9 @@ fun DinerGateApp(
 
             composable(route = DinerGateScreen.ShopDetail.name) {
                 ShopDetailScreen(
-                    details = uiState.hotPepperApiResult.results.shop[uiState.shopFocusIndex]
+                    details = uiState.hotPepperApiResult.results.shop[uiState.shopFocusIndex],
+                    lat = uiState.currentLatitude,
+                    lng = uiState.currentLongitude
                 )
             }
         }
