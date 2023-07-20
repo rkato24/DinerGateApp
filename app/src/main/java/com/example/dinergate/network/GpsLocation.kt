de86333570a673019264ef09a8ad170104e0ad2c
rkato24 <ryokakato24@gmail.com>
2023-07-20 16:05:42 +0900

    外部から緯度・経度を取得できるように修正（途中）

--- app/src/main/java/com/example/dinergate/network/GpsLocation.kt
+++ app/src/main/java/com/example/dinergate/network/GpsLocation.kt
@@ -1,94 +1,108 @@
 package com.example.dinergate.network
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.view.WindowManager
+import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.*
-
-class GpsLocation : AppCompatActivity() {
-    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
-
-    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
-    private lateinit var lastLocation: Location
-    private lateinit var locationCallback : LocationCallback
-    private lateinit var _locationRequest: LocationRequest
-
-    public var lat: Double = 0.0
-    public var lng: Double = 0.0
+import androidx.core.content.ContextCompat
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+class GpsLocation : AppCompatActivity(), LocationListener {
+
+    // lateinit: late initialize to avoid checking null
+    private lateinit var locationManager: LocationManager
+    private var returnLocation: Location? = null
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // 使用が許可された
+            returnLocation = locationStart()
+
+        } else {
+            // それでも拒否された時の対応
+            val toast = Toast.makeText(this,
+                "これ以上なにもできません", Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        _locationRequest = LocationRequest.create()
 
-        checkPermission()
-
-    }
-
-    private fun checkPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED){
-            myLocationEnable()
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         } else {
-            requestLocationPermission()
+            returnLocation = locationStart()
         }
-
     }
 
-    private fun requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // 許可を求め、拒否されていた場合
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+    private fun locationStart() :Location? {
+        Log.d("debug", "locationStart()")
+
+        // Instances of LocationManager class must be obtained using Context.getSystemService(Class)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled")
         } else {
-            // まだ許可を求めていない
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
-
+            // to prompt setting up GPS
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
         }
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
-            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION->{
-                if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 許可された
-                    myLocationEnable()
-                } else {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
 
-                }
-            }
+            Log.d("debug", "checkSelfPermission false")
+            return null
         }
-    }
-    private fun myLocationEnable(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-            val locationRequest =  LocationRequest().apply {
-                interval = 10000        //最も長い更新時間
-                fastestInterval = 5000  //最も短い更新時間
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            }
-            locationCallback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult) {
-                    //lastLocation = locationResult.lastLocation
-                    lat = lastLocation.latitude
-                    lng = lastLocation.longitude
-                    }
-                }
-            }
-            fusedLocationProviderClient.requestLocationUpdates(_locationRequest,locationCallback,null)
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000,
+            50f,
+            this)
+
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
     }
 
-    fun getGpsLocation (): Pair<Double, Double> {
-        return Pair(lat, lng)
+    fun getCurrentLocation(): Location? {
+        return returnLocation
+    }
+
+
+
+    override fun onLocationChanged(location: Location) {
+
+    }
+
+
+    override fun onProviderEnabled(provider: String) {
+
+    }
+
+    override fun onProviderDisabled(provider: String) {
+
     }
 
 }
