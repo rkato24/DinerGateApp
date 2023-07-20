8c8537683656eea4c80346deeb3fe10662d7dff4
rkato24 <ryokakato24@gmail.com>
2023-07-20 17:24:49 +0900

    GpsLocationのインスタンス化を削除 Spacerの追加 StartScreen, onSearchButtonClickedの引数のうち使用されていないものを削除 コメントの追加・修正

--- app/src/main/java/com/example/dinergate/ui/theme/screens/StartScreen.kt
+++ app/src/main/java/com/example/dinergate/ui/theme/screens/StartScreen.kt
@@ -1,39 +1,18 @@
 package com.example.dinergate.ui.theme.screens
 
-import android.widget.ArrayAdapter
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.dinergate.R
-import com.example.dinergate.model.HotPepperApiResult
-import com.example.dinergate.model.Shop
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import android.app.Application
-import android.content.Context
-import android.location.Location
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.RadioButton
@@ -42,34 +21,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.dinergate.dummyResult
-import com.example.dinergate.network.GpsLocation
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationServices
+
+/*
+
+StartScreen : アプリ開始時の検索範囲を指定する画面コンポーザブル
+ラジオボタンで検索範囲を指定させたのち、指定された検索範囲に対応するインデックスをviewModelに渡す。
+検索ボタンが押されると、指定された検索範囲と取得された緯度経度に基づいてAPIコールを行い、
+結果をもとに検索結果画面(SearchResultScreen)に遷移する。
+
+ */
 
 @Composable
 fun StartScreen(
-    hotPepperApiResult: HotPepperApiResult,
-    onSearchButtonClicked: (HotPepperApiResult) -> Unit,
-    onSelectionChanged: (Int) -> Unit = {},
-    Id: Int,
-    modifier: Modifier = Modifier.padding(32.dp)
+    modifier: Modifier = Modifier,
+    onSearchButtonClicked: () -> Unit,
+    onSelectionChanged: (Int) -> Unit = {}
 ) {
-    val gpsLocation = GpsLocation()
 
     var selectedValue by rememberSaveable { mutableStateOf("") }
+    val rangeID = listOf("300m", "500m", "1000m", "2000m", "3000m")
 
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-        Text(text = "Welcome to DinerGate")
+        Text(
+            text = "Welcome to DinerGate",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+            )
+        Spacer(Modifier.height(16.dp))
         Text(text = "近くのレストランを検索します")
         Text(text = "現在地からの検索半径を指定してください")
-        //Spacer(modifier = Modifier.height(16.dp ))
-        val rangeID = listOf("300m", "500m", "1000m", "2000m", "3000m")
+        Spacer(Modifier.height(16.dp ))
         rangeID.forEachIndexed { index, item ->
             Row(
                 modifier = Modifier.selectable(
@@ -91,41 +80,16 @@ fun StartScreen(
                 Text(item)
             }
         }
-        Text(text = Id.toString())
+        Spacer(Modifier.height(16.dp ))
         SearchButton(
-            onClick = { onSearchButtonClicked(hotPepperApiResult) }
+            onClick = { onSearchButtonClicked() }
         )
-        //Text(text = gpsLocation.getGpsLocation().toString())
     }
 
 }
 
-/**
- *
- * @OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SearchKeywordField() {
-OutlinedTextField (
-value = "",
-onValueChange = {}
-)
-}
-
-
-val adapter = ArrayAdapter<CharSequence>(
-this,
-R.array.rangeSpinnerItems,
-android.R.layout.simple_spinner_item
-)
- *
- *
- *
- *
- */
-
 @Composable
 fun SearchButton(
-
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -143,11 +107,9 @@ fun SearchButton(
 fun StartScreenPreview(){
     StartScreen(
 
-        hotPepperApiResult = dummyResult,
 
         onSearchButtonClicked = {},
         onSelectionChanged = {},
-        Id = 3,
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
