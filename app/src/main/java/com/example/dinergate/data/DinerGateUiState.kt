180a6a93c8a1411df336ffea7d75f6de005ce301
rkato24 <ryokakato24@gmail.com>
2023-07-20 16:03:02 +0900

    コメントで解説を追加

--- app/src/main/java/com/example/dinergate/data/DinerGateUiState.kt
+++ app/src/main/java/com/example/dinergate/data/DinerGateUiState.kt
@@ -1,15 +1,40 @@
 package com.example.dinergate.data
 
+import com.example.dinergate.dummyResult
 import com.example.dinergate.model.HotPepperApiResult
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+
+/*
+
+アプリ全体にわたって保持しておく状態のデータクラス
+
+val hotPepperApiResult: HotPepperApiResult = dummyResult
+    グルメサーチAPIから得られたデータをHotPepperApiResultデータクラス形式に変換したもの
+    ダミーデータで初期化→setHotPepperApiResultで更新される
+val shopFocusIndex: Int = 0,
+    検索結果画面で、表示されるList<Shop>のうち何番目のものが選択されたかのインデックスを保持する
+    検索結果画面で詳細画面に移動する操作が行われるたびにsetShopFocusIndexで更新される
+val currentLatitude: Double = 34.8424,
+val currentLongitude: Double = 135.7895,
+    それぞれGpsLocationで得られた端末の緯度・経度を保持する
+    setCurrentLatitude, setCurrentLongitudeで更新可能
+val rangeId: Int = 3
+    スタート画面で、指定された検索半径に対応するインデックスを保持する
+    対応関係は
+    ・1→300m
+    ・2→500m
+    ・3→1000m
+    ・4→2000m
+    ・5→3000m
+    初期値は3で、ラジオボタンが選択されていない状態で検索ボタンが押されると
+    半径1000mを選択して計算したこととなる
+    （初期状態で1000mが選択されているようになるのが望ましい）
+
+ */
 
 data class DinerGateUiState (
-    val hotPepperApiResult: HotPepperApiResult,
+    val hotPepperApiResult: HotPepperApiResult = dummyResult,
     val shopFocusIndex: Int = 0,
-    //val searchResultPage: Int = 1
-    //val currentLatitude: Double = 34.8424,
-    val currentLatitude: Double = 34.00,
+    val currentLatitude: Double = 34.8424,
     val currentLongitude: Double = 135.7895,
     val rangeId: Int = 3
 )
